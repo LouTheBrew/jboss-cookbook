7b7714225d9062b0b1c94c4ae7d8e48e1f5b80aa
Luis De Siqueira <louthebrew@gmail.com>
2016-05-05 15:17:42 -0700

    committing for release

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,3 @@
-#
-# Cookbook Name:: jboss-cookbook
-# Recipe:: default
-#
-# Copyright (c) 2016 The Authors, All Rights Reserved.
-include_recipe 'java'
-poise_service_user 'jboss'
 jboss_app 'core01' do
   installation_dir '/opt/jboss/4.2.3.GA/server/'
   owner 'jboss'
