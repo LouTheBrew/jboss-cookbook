1f12889263eee0491be56643e386f84ed5a40a40
Luis De Siqueira <louthebrew@gmail.com>
2016-05-25 16:26:13 -0700

    upping version removing default action

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'LouTheBrew@gmail.com'
 license 'MIT'
 description 'resources for controlling jboss'
 long_description 'resources for controlling jboss'
-version '0.0.3'
+version '0.0.4'
 
 depends 'poise'
 depends 'poise-service'
